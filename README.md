9db9769b8e0cc8853dd5687df3a0a55a9c1cd44f
Gavin Rehkemper <gavin@gavinr.com>
2022-06-07 00:05:00 -0500

    demo files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OAuth Demo - ArcGIS REST JS
+
+See [this demo](https://github.com/Esri/arcgis-rest-js/tree/main/demos/oauth2-browser) for a similar simple (but slightly more complex) example.
---
+++ main.js
@@ -0,0 +1,30 @@
+// https://developers.arcgis.com/applications/4ddac6b970dd408081d348d0d2ad870e/
+const CLIENT_ID = "WiVUfuC2NyQC06zA";
+
+let opts = {
+  clientId: CLIENT_ID,
+  popup: false,
+  redirectUri: window.location.href,
+};
+
+// After the user is redirected here, this will run
+arcgisRest.ArcGISIdentityManager.completeOAuth2(opts)
+  .then((newSession) => {
+    console.log("DONE", newSession.username);
+    resultsDomNode.innerHTML = newSession.serialize();
+  })
+  .catch((e) => {
+    console.log("Note:", e);
+  });
+
+// When the login button is clicked, call beginOAuth2:
+loginButton.addEventListener("click", () => {
+  console.log("login button click");
+  arcgisRest.ArcGISIdentityManager.beginOAuth2(opts)
+    .then(() => {
+      console.log("going....");
+    })
+    .catch((e) => {
+      console.log("Error", e);
+    });
+});
